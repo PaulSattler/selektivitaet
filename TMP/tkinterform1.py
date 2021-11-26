0148fdd9b4d1b3798e4028ea475a0b1df87aa5c0
Paul Sattler <ps.paulsattler@gmx.de>
2021-11-26 18:22:13 +0100

    gui fertig :)

--- TMP/tkinterform1.py
+++ TMP/tkinterform1.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog as fd
 from array import array
+import os
 
 
 OptionList = [
@@ -17,6 +18,13 @@ def getFilepath(a, b):
     fileinputstack[a][b].config(text=filename)
     #return filename
 
+def getFilepathSchmelz(a):
+    filename = fd.askopenfilename()
+    if len(filename)<2:
+        filename = "Hier ist etwas schief gelaufen.."
+    schmelzsicherungstack[a].config(text=filename)
+    #return filename
+
 #root = Hauptfenster
 root = tk.Tk()
 root.geometry('550x800')
@@ -30,6 +38,68 @@ buttonplusstack = []
 buttonminusstack = []
 variablestack = []
 
+schmelzsicherungstack = [] 
+schmelzsicherungnamestack = []
+
+konstantstackx = []
+konstantstacky = []
+konstantstackf = []
+
+
+def destroyLinePart(line):
+    #line = line + 1
+    print(line)
+    fileinputstack[0][line-1].grid_remove()
+    fileinputstack[1][line-1].grid_remove()
+    nameinputstack[0][line-1].grid_remove()
+    nameinputstack[1][line-1].grid_remove()
+    ininputstack[0][line-1].grid_remove()
+    ininputstack[1][line-1].grid_remove()
+    schmelzsicherungnamestack[line-1].grid_remove()
+    schmelzsicherungstack[line-1].grid_remove()
+    konstantstackx[line-1].grid_remove()
+    konstantstacky[line-1].grid_remove()
+    konstantstackf[line-1].grid_remove()
+
+def Schmelzsicherung(line):
+    print("Schmelzsicherung Line: {}".format(line))
+    destroyLinePart(line)
+    print(schmelzsicherungnamestack)
+    schmelzsicherungnamestack[line-1].grid(row=((line-1)*3)+1, column=2)
+    schmelzsicherungstack[line-1].grid(row=((line-1)*3), column=1)
+
+def Konstante(line):
+    print("Konstante Line: {}".format(line))
+    destroyLinePart(line)
+    konstantstackx[line-1].grid(row=((line-1)*3), column=1)
+    konstantstacky[line-1].grid(row=((line-1)*3)+1, column=1)
+    konstantstackf[line-1].grid(row=((line-1)*3), column=2)
+    konstantstackx[line-1].insert(0, "X")
+    konstantstacky[line-1].insert(0, "Y")
+    konstantstackf[line-1].insert(0, "Function")
+    return 
+
+def newFileinputSchmelzsicherung(n):
+    n = int(n)
+    label = tk.Label(root, text="Geth file path")
+    label.bind("<Button-1>", lambda e:getFilepathSchmelz(n))
+    schmelzsicherungstack.append(label)
+
+def newFileinputSchmelzsicherungName():
+    myInput = tk.Entry(root, width=40)
+    schmelzsicherungnamestack.append(myInput)
+
+def newKonstantx():
+    myInput = tk.Entry(root, width=5)
+    konstantstackx.append(myInput)
+
+def newKonstanty():
+    myInput = tk.Entry(root, width=5)
+    konstantstacky.append(myInput)
+
+def newKonstantf():
+    myInput = tk.Entry(root, width=20)
+    konstantstackf.append(myInput)
 
 def newLabel(name):
     global labelstack
@@ -67,26 +137,39 @@ def newIN():
 
 def plusbutton_pressed():
     print(labelstack)
-    addNewLine(len(labelstack)+1)
+    addNewLine(len(labelstack))
 
 def minusbutton_pressed(line):
     #todo ungrid all in line
     if line > 1:
-        labelstack[line-1].destroy()
-        dropdownstack[line-1].destroy()
-        fileinputstack[0][line-1].destroy()
-        fileinputstack[1][line-1].destroy()
-        nameinputstack[0][line-1].destroy()
-        nameinputstack[1][line-1].destroy()
-        ininputstack[0][line-1].destroy()
-        ininputstack[1][line-1].destroy()
-        buttonplusstack[line-1].destroy()
-        buttonminusstack[line-1].destroy()
+        labelstack[line-1].grid_remove()
+        dropdownstack[line-1].grid_remove()
+        buttonplusstack[line-1].grid_remove()
+        buttonminusstack[line-1].grid_remove()
+        fileinputstack[0][line-1].grid_remove()
+        fileinputstack[1][line-1].grid_remove()
+        nameinputstack[0][line-1].grid_remove()
+        nameinputstack[1][line-1].grid_remove()
+        ininputstack[0][line-1].grid_remove()
+        ininputstack[1][line-1].grid_remove()
+        schmelzsicherungnamestack[line-1].grid_remove()
+        schmelzsicherungstack[line-1].grid_remove()
+        konstantstackx[line-1].grid_remove()
+        konstantstacky[line-1].grid_remove()
+        konstantstackf[line-1].grid_remove()
+
+
     #print(str(line))
     #print("labelstack"+str(labelstack),    "dropdownstack"+str(dropdownstack),    "fileinputstack"+str(fileinputstack),    "nameinputstack"+str(nameinputstack),    "ininputstack"+str(ininputstack),    "buttonplusstack"+str(buttonplusstack),    "buttonminusstack"+str(buttonminusstack))
     return
 
 def makenewform(masterrow, function):
+    if function == "Schmelzsicherung":
+        Schmelzsicherung(masterrow)
+    elif function == "Leitungsschutzschalter":
+        Sicherungsautomat(masterrow, True)           ###############################
+    elif function == "Funktion":
+        Konstante(masterrow)
     print(masterrow, function)
     return
 
@@ -97,7 +180,7 @@ def dropdownchanged(*args):
     else: row = int(str(args[0][len(args[0])-2]) +  str(args[0][len(args[0]) - 1]))
     #labelstack[row].destroy()
     #print(variablestack[row].get())
-    makenewform(row, variablestack[row].get())
+    makenewform(row+1, variablestack[row].get())
     return
 
 def newPlusbutton():
@@ -109,31 +192,41 @@ def newMinusbutton(line):
     buttonminusstack.append(mybutton)
 
 def addNewLine(line):
+    line = line + 1
     newLabel("Sicherung " + str(line) + ":")
-    labelstack[len(labelstack)-1].grid(row=(line-1)*3, column=0)
-
-    newDropdown(line)
-    dropdownstack[len(dropdownstack)-1].grid(row=((line-1)*3)+1, column=0)
-
-    newFileinput(0)
-    fileinputstack[0][len(fileinputstack[0])-1].grid(row=(line-1)*3, column=1)
-    fileinputstack[1][len(fileinputstack[1])-1].grid(row=((line-1)*3)+1, column=1)
-    
+    newDropdown(line)   
+    newFileinput(line-1)       # line-1 OR 0 #######################################################################################################
     newName()
-    nameinputstack[0][len(nameinputstack[0])-1].grid(row=(line-1)*3, column=2)
-    nameinputstack[1][len(nameinputstack[1])-1].grid(row=((line-1)*3)+1, column=2)
-
     newIN()
-    ininputstack[0][len(ininputstack[0])-1].grid(row=(line-1)*3, column=3)
-    ininputstack[0][len(ininputstack[0])-1].insert(0, "I/A")
-    ininputstack[1][len(ininputstack[1])-1].grid(row=((line-1)*3)+1, column=3)
-    ininputstack[1][len(ininputstack[1])-1].insert(0, "I/A")
-    
     newPlusbutton()
-    buttonplusstack[line-1].grid(row=((line-1)*3)+2, column=3)
-
     newMinusbutton(line)
+    newFileinputSchmelzsicherungName()
+    newFileinputSchmelzsicherung(line-1)
+    newKonstantx()
+    newKonstanty()
+    newKonstantf()
     buttonminusstack[line-1].grid(row=((line-1)*3)+2, column=2, sticky=tk.E)
+    buttonplusstack[line-1].grid(row=((line-1)*3)+2, column=3)
+    dropdownstack[line-1].grid(row=((line-1)*3)+1, column=0)
+    Sicherungsautomat(line, False)
+
+def Sicherungsautomat(line, change):
+    if change == True:
+        destroyLinePart(line)
+    print("Sicherungsautomat Line: {}".format(line))
+    linerow = line #################################################
+    labelstack[line-1].grid(row=(linerow-1)*3, column=0)
+    fileinputstack[0][line-1].grid(row=(linerow-1)*3, column=1)
+    fileinputstack[1][line-1].grid(row=((linerow-1)*3)+1, column=1)
+    nameinputstack[0][line-1].grid(row=(linerow-1)*3, column=2)
+    nameinputstack[1][line-1].grid(row=((linerow-1)*3)+1, column=2)
+    ininputstack[0][line-1].grid(row=(linerow-1)*3, column=3)
+    ininputstack[1][line-1].grid(row=((linerow-1)*3)+1, column=3)
+    if change == False:
+        ininputstack[0][line-1].insert(0, "I/A")
+        ininputstack[1][line-1].insert(0, "I/A")
+
+    return
 
     
 
@@ -142,11 +235,7 @@ def addNewLine(line):
 
 
 
-
-
-
-
-
+addNewLine(0)
 addNewLine(1)
 #Starte mainloop
 root.mainloop()
